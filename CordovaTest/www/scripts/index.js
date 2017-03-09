3f5a19ab7f4c2c2a5de981b9ef5d907e66f57802
karimabd84 <abdelkarim.esti@gmail.com>
2017-03-09 13:01:13 +0100

    binding of an external website by apache cordova

--- CordovaTest/www/scripts/index.js
+++ CordovaTest/www/scripts/index.js
@@ -1,4 +1,6 @@
-﻿// Eine Einführung zur leeren Vorlage finden Sie in der folgenden Dokumentation:
+﻿/// <reference path="scripts/jquery-3.1.1.js" />
+var appUrl = 'https://app.pacti.de';
+// Eine Einführung zur leeren Vorlage finden Sie in der folgenden Dokumentation:
 // http://go.microsoft.com/fwlink/?LinkID=397704
 // Zum Debuggen von Code beim Laden einer Seite in cordova-simulate oder auf Android-Geräten/-Emulatoren: App starten, Haltepunkte festlegen, 
 // und führen Sie dann "window.location.reload()" in der JavaScript-Konsole aus.
@@ -18,6 +20,20 @@
         var receivedElement = parentElement.querySelector('.received');
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
+
+        var url = appUrl + "/api/setphonegap?deviceuid=" + device.uuid +
+            "&devicename=" + device.model +
+            "&phonegap=3.6.3" +
+            "&deviceos=" + device.platform +
+            "&deviceversion=" + device.version;
+
+        console.log(url);
+
+        $('html').load(encodeURI(url));
+        
+
+        console.log(device.uuid);
+
     };
 
     function onPause() {
